cb10d3d70ad0ad3fd9108295d2c6a41c3a16ef01
Stephanie Ortiz <stephthedev@gmail.com>
2020-11-26 15:50:41 -0500

    Update sea travel caption

--- src-react/components/calculator/travel/sea/SeaTravelCaption.js
+++ src-react/components/calculator/travel/sea/SeaTravelCaption.js
@@ -17,32 +17,32 @@ export default class SeaTravelCaption extends Component {
         </caption>
         <thead>
           <th>Ship</th>
-          <th>Distance Per Day</th>
+          <th>Miles Per Day</th>
         </thead>
         <tbody>
           <tr>
-            <td>Galley</td>
-            <td>96 miles</td>
+            <td>Rowboat</td>
+            <td>24</td>
           </tr>
           <tr>
             <td>Keelboat</td>
-            <td>72 miles</td>
+            <td>72</td>
           </tr>
           <tr>
-            <td>Longship</td>
-            <td>120 miles</td>
-          </tr>
-          <tr>
-            <td>Rowboat</td>
-            <td>24 miles</td>
-          </tr>
-          <tr>
-            <td>Sailing Ship</td>
-            <td>120 miles</td>
+            <td>Galley</td>
+            <td>96</td>
           </tr>
           <tr>
             <td>Warship</td>
-            <td>96 miles</td>
+            <td>96</td>
+          </tr>
+          <tr>
+            <td>Longship</td>
+            <td>120</td>
+          </tr>
+          <tr>
+            <td>Sailing Ship</td>
+            <td>120</td>
           </tr>
         </tbody>
       </table>
